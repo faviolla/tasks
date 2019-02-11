a5c7cb351aa15a25e20390da3ee867e00480eb3f
Anzhelika <anjelika.belikh@gmail.com>
2019-02-11 19:55:10 +0200

    trying to return created DOM elements

--- hw_12/js/news.ui.service.js
+++ hw_12/js/news.ui.service.js
@@ -9,7 +9,7 @@ class NewsUI {
      */
     addArticle(article) {
         // console.time();
-        const template = NewsUI.generateArticleTemplate(article);
+        const template = (NewsUI.generateArticleTemplate(article)).toString();
         // console.timeEnd();
         this.newsContainer.insertAdjacentHTML("afterbegin", template);
     }
@@ -28,6 +28,38 @@ class NewsUI {
      * @param {Object} article 
      */
     static generateArticleTemplate(article) {
+        
+        // let parentEl = document.createElement('div');
+        // parentEl.setAttribute('class', 'col s12 m6');
+        // let card = document.createElement('div');
+        // card.setAttribute('class', 'card');
+        // parentEl.appendChild(card);
+        // let cardImage = document.createElement('div');
+        // cardImage.setAttribute('class', 'card-image');
+        // cardImage.innerHTML = '<img src="' + article.urlToImage + '">'
+        // card.appendChild(cardImage);
+        // let cardContent = document.createElement('div');
+        // cardContent.setAttribute('class', 'card-content');
+        // card.appendChild(cardContent);
+        // let cardTitle = document.createElement('span');
+        // cardTitle.setAttribute('class', 'card-title');
+        // cardTitle.innerHTML = article.title || '' ;
+        // cardContent.appendChild(cardTitle);
+        // let p = document.createElement('p');
+        // p.innerHTML = article.description || '' ;
+        // cardContent.appendChild(p);
+        // let cardAction = document.createElement('div');
+        // cardAction.setAttribute('class', 'card-action');
+        // let link = document.createElement('a');
+        // link.setAttribute('href', article.url);
+        // link.setAttribute('target', '_blank');
+        // link.innerHTML = 'Read more';
+        // cardAction.appendChild(link);
+        // card.appendChild(cardAction);
+
+        // console.log(parentEl);
+        // return parentEl;
+        
         return `
         <div class="col s12 m6">
             <div class="card">
